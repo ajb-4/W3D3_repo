09beaec4f5f55fff6804b310ca247381bc0871ae
ajb-4 <alexbrown4711@gmail.com>
2023-07-12 13:49:31 -0400

    done with range

--- Recursion/Recursion.rb
+++ Recursion/Recursion.rb
@@ -1,8 +1,26 @@
 
 def range(start, last)
   return [] if last <= start #base case
-  #return [start] if last == start
 
-
+  [start] + range(start + 1, last)
 
 end
+
+# p range(1,5)
+# p range(2,5)
+# p range(5,2)
+# p range(5,5)
+
+def it_range(start, last)
+new_array = []
+  (start...last).each do |num|
+      new_array << num
+  end
+new_array
+end
+
+p it_range(1,5)
+p it_range(2,5)
+p it_range(5,2)
+p it_range(5,5)
+
